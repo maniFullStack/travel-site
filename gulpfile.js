b5dd097d5d398ea90b21128ca5c6eaeb0872146b
maiFullStack <isoty.mani@gmail.com>
2018-06-05 01:21:00 -0400

    css organize

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var postcss =require('gulp-postcss');
+var autoProfixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssImport =require('postcss-import');
 
 gulp.task('default',()=>{
 console.log('Gulp Task Created!!!!');
@@ -11,18 +16,23 @@ console.log('May be you can edit html here....');
 
 
 
-gulp.task('styles',()=>{
-    console.log('May be you can edit CSS here....');
+gulp.task('styles',function(){
+    console.log('CSSS test');
+return gulp.src('./app/assets/styles/styles.css')
+.pipe(postcss([cssImport,cssvars,nested,autoProfixer]))
+.pipe(gulp.dest('./app/temp/styles'));
+
+
     });
     
 
 
-gulp.task('watch',()=>{
+gulp.task('watch',function(){
 watch('./app/index.html',function(){
 gulp.start('html');
 });
 
-watch('./app/assets/..',function(){
+watch('./app/assets/styles/..',function(){
     gulp.start('styles');
     });
     
